a56aacb42135bf3968a06c050c08b26be32d1373
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2026-04-22 12:29:09 +0200

    Remove unused tensor_legs_to_digit method
    The method was used to convert integer inputs to char inputs. However,
    users probably have their own tensor structure and need to convert it to
    this format anyway, so they can directly go to char vecs instead of
    first to int vecs and then use this method.

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,3 @@
-use std::iter::zip;
-
 use rustc_hash::FxHashMap;
 
 const BASE_SYMBOLS: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,30 +20,6 @@ pub fn get_symbol(leg: usize) -> char {
     } else {
         char::from_u32((leg + 140).try_into().unwrap()).unwrap()
     }
-}
-
-/// Converts tensor leg inputs (as usize) to chars. Creates new inputs, outputs and size_dict that can be fed to Cotengra.
-pub fn tensor_legs_to_digit(
-    inputs: &[Vec<usize>],
-    outputs: &[usize],
-    size_dict: &FxHashMap<usize, u64>,
-) -> (Vec<Vec<char>>, Vec<char>, FxHashMap<char, u64>) {
-    let mut new_inputs = vec![Vec::new(); inputs.len()];
-    let mut new_size_dict = FxHashMap::default();
-
-    for (tensor, new_tensor) in zip(inputs.iter(), new_inputs.iter_mut()) {
-        new_tensor.reserve_exact(tensor.len());
-        for leg in tensor {
-            let string_value = get_symbol(*leg);
-            new_tensor.push(string_value.clone());
-            new_size_dict.insert(string_value, size_dict[leg]);
-        }
-    }
-    (
-        new_inputs,
-        outputs.iter().copied().map(get_symbol).collect(),
-        new_size_dict,
-    )
 }
 
 /// Converts path from SSA to replace left path format.
@@ -101,51 +75,4 @@ pub fn replace_to_ssa_path(
         *j = right_id;
     }
     replace_path
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_tensor_inputs_to_string() {
-        let inputs = vec![vec![1505, 0, 2, 1], vec![4, 3, 2, 1], vec![4, 3, 5, 6]];
-        let outputs = vec![5, 6];
-        let size_dict = FxHashMap::from_iter([
-            (1505, 4),
-            (0, 5),
-            (1, 6),
-            (2, 7),
-            (3, 8),
-            (4, 9),
-            (5, 10),
-            (6, 11),
-        ]);
-
-        let (new_inputs, new_outputs, new_size_dict) =
-            tensor_legs_to_digit(&inputs, &outputs, &size_dict);
-
-        assert_eq!(
-            new_inputs,
-            vec![
-                vec!['\u{066D}', 'a', 'c', 'b'],
-                vec!['e', 'd', 'c', 'b'],
-                vec!['e', 'd', 'f', 'g']
-            ]
-        );
-        assert_eq!(new_outputs, vec!['f', 'g']);
-        assert_eq!(
-            new_size_dict,
-            FxHashMap::from_iter([
-                ('\u{066D}', 4),
-                ('a', 5),
-                ('b', 6),
-                ('c', 7),
-                ('d', 8),
-                ('e', 9),
-                ('f', 10),
-                ('g', 11),
-            ])
-        );
-    }
 }
